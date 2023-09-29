51597a6518c0213ce7084499d6d366c9b0adb3c7
Raveesh Samkaria <rishu9417@gmail.com>
2023-09-29 21:19:29 +0530

    Design Patterns - Ques 8, 9  & 10

---
+++ DesignPatterns/Q9.cpp
@@ -0,0 +1,20 @@
+// Print the following pattern
+// a b c d e
+// a b c d e
+// a b c d e 
+// a b c d e
+// a b c d e
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int row;
+	char col;
+    for(row=1; row<=5; row++) {
+        for(col='a'; col<='e'; col++) {
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+}
